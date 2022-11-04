a094e6f317d25ea6549d81a4955da52fc4eb7f45
jomarski <116093622+jomarski@users.noreply.github.com>
2022-11-04 21:57:54 +0800

    Add files via upload

---
+++ MERN App.js (Batch 1)/Lists in React/App4.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      ...
+   
+      <hr />
+      <ul>
+        {list.map(function (item) {
+          return <li key={item.objectID}>{item.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
